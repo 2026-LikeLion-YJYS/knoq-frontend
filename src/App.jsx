d91674fd168d8b264cb64ae00a9bb83c7babad90
veeesong <yunseosong66@gmail.com>
2026-08-17 06:00:01 +0900

    feat: 약관 동의 확인 화면 아이콘/간격/그라데이션 수정 #6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-// [추가] 온보딩 화면 확인용
-import Onboarding1 from "./pages/onboarding/Onboarding1";
+// [추가] 온보딩2 화면 확인용
+import Onboarding2 from "./pages/onboarding/Onboarding2";
+
+// [테스트용, 나중에 라우터로 대체] 온보딩1 확인용
+// import Onboarding1 from "./pages/onboarding/Onboarding1";
 
 // [테스트용, 나중에 라우터로 대체] 헤더/네비바 확인용
 // import MainHeader from "./components/MainHeader/MainHeader";
@@ -11,11 +14,19 @@ function App() {
   // const [activeTab, setActiveTab] = useState("analysis");
 
   return (
-    <Onboarding1
-      onSelectPrivate={() => alert("프라이빗 선택")}
-      onSelectAccount={() => alert("계정 선택")}
+    <Onboarding2
+      onBack={() => alert("뒤로가기")}
+      onSubmit={(consents) => console.log("제출된 동의값:", consents)}
     />
   );
+
+  // [테스트용, 나중에 라우터로 대체] 온보딩1 화면
+  // return (
+  //   <Onboarding1
+  //     onSelectPrivate={() => alert("프라이빗 선택")}
+  //     onSelectAccount={() => alert("계정 선택")}
+  //   />
+  // );
 
   // [테스트용, 나중에 라우터로 대체] 헤더/네비바 화면
   // return (
