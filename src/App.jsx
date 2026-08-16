a4332ca42f83f6a48988275a6a70a9b98428f87f
xhar1otte <charlottelog@naver.com>
2026-08-16 21:47:33 +0900

    feat: 직원 로그인 진입 화면 구현 #8

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Help from "./pages/help/Help";
 import Onboarding1 from "./pages/onboarding/Onboarding1";
 
-function App() {
-  const CURRENT_PAGE = "help"; // "onboarding" 또는 "help"로 변경하여 페이지 전환
+// [추가] 직원 로그인 진입 화면
+import StaffIntro from "./pages/staff/StaffIntro";
 
+function App() {
+  // [수정] "onboarding", "help", "staffIntro"로 변경하여 화면 확인
+  const CURRENT_PAGE = "staffIntro";
+
+  // 온보딩 화면
   if (CURRENT_PAGE === "onboarding") {
     return (
       <Onboarding1
@@ -13,6 +18,19 @@ function App() {
     );
   }
 
+  // [추가] 직원 로그인 진입 화면
+  if (CURRENT_PAGE === "staffIntro") {
+    return (
+      <StaffIntro
+        onLogin={() => {
+          // [추가] StaffLogin 구현 전 임시 버튼 동작
+          alert("직원 로그인 화면으로 이동합니다.");
+        }}
+      />
+    );
+  }
+
+  // 도움 화면
   return <Help />;
 }
 
